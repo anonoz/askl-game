4d8e5fb00cd17e6a04471b72c6d1a430e01d184f
Anonoz Chong <hello@anonoz.com>
2024-04-21 16:18:47 +0800

    Animate balls flying down

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,48 @@
 <script lang="ts">
+	// Libs
 	import { onMount, tick } from 'svelte';
+	import { writable } from 'svelte/store';
+	import { fade, fly } from 'svelte/transition';
+
+	// Image assets
 	import imgHitpad from '$lib/images/hitpad@3x.png';
-	import Page from './sverdle/how-to-play/+page.svelte';
+	import imgBallLane1 from '$lib/images/ball-lane-1@3x.png';
+	import imgBallLane2 from '$lib/images/ball-lane-2@3x.png';
+	import imgBallLane3 from '$lib/images/ball-lane-3@3x.png';
+	import imgBallLane4 from '$lib/images/ball-lane-4@3x.png';
+	import Counter from './Counter.svelte';
+	import { cubicIn } from 'svelte/easing';
 
 	const keyA = 65;
 	const keyS = 83;
 	const keyK = 75;
 	const keyL = 76;
 	const keySpace = 32;
-	let chartCsv: Object = {};
+	let chartCsv: String;
 
 	const graphicSettings = {
 		beatDuration: 3, // seconds
-		hitpadOffset: [116 / 2, 62 / 2]
+		hitpadOffset: [116 / 2, 62 / 2],
+		laneBallOffset: 0
 	};
 	const lanesGraphics = [
 		{
+			ball_graphic: imgBallLane1,
 			start_xy: [100, 100],
 			end_xy: [100, 600]
 		},
 		{
+			ball_graphic: imgBallLane2,
 			start_xy: [300, 100],
 			end_xy: [300, 600]
 		},
 		{
+			ball_graphic: imgBallLane3,
 			start_xy: [500, 100],
 			end_xy: [500, 600]
 		},
 		{
+			ball_graphic: imgBallLane4,
 			start_xy: [700, 100],
 			end_xy: [700, 600]
 		}
@@ -40,7 +55,8 @@
 	let currentScore = 0;
 
 	// Each lane gets their own id => timings[]
-	let beatsStack = {};
+	let beatsStack: Array<Array<Number>> = {};
+	let ballsInLane = [[], [], [], []];
 
 	let audioPlayer: HTMLMediaElement;
 
@@ -52,14 +68,33 @@
 
 	// This doesn't start the game
 	function setupGame() {
+		console.log('Running setupGame();');
 		// Load the beats stack
-		beatsStack = parseCSV(chartCsv);
+		beatsStack = parseChartCSV(chartCsv);
+		console.log(beatsStack);
 
 		// Reset game states
 		currentScore = 0;
 		audioPlayer.fastSeek(0);
 		audioPlayer.play();
-		// Bind rendering event
+
+		// // Bind rendering event
+		for (let lane_id in beatsStack) {
+			for (let timing of beatsStack[lane_id]) {
+				setTimeout(
+					() => {
+						ballsInLane[lane_id] = ballsInLane[lane_id] + [timing * 1000];
+						console.log(`Adding ball ${lane_id} for time ${timing * 1000}`);
+
+						setTimeout(() => {
+							console.log(`Removing ball ${lane_id} #${timing * 1000}`);
+							ballsInLane[lane_id] = ballsInLane[lane_id].slice(1);
+						}, 500);
+					},
+					1000.0 * (timing - graphicSettings.beatDuration)
+				);
+			}
+		}
 	}
 
 	// Set timeout
@@ -81,6 +116,8 @@
 		// Space bar starts-pauses the game
 		if (e.keyCode == keySpace) {
 			if (wallTimeStartedAt == 0 && gameTimePausedAt == 0) {
+				setupGame();
+				return;
 				// Start game
 				wallTimeStartedAt = Date.now();
 				audioPlayer.play();
@@ -117,22 +154,24 @@
 
 	// Utilities
 	// Simple CSV Parser specific to your data structure
-	function parseCSV(csv) {
+	function parseChartCSV(csv: String) {
 		let lines = csv.split('\n');
-		let result = {};
+		let result: Array<Array<Number>> = [];
 		// Skip header line, iterate from second line
 		for (let i = 1; i < lines.length; i++) {
 			if (!lines[i]) continue;
 			const [strikeAt, lane] = lines[i].split(',');
-			const key = `lane_${lane.trim()}`;
-			if (!result[key]) {
-				result[key] = [];
+			const lane_id = parseInt(lane.trim()) - 1;
+			if (!result[lane_id]) {
+				result[lane_id] = [];
 			}
-			result[key].push(parseFloat(strikeAt.trim()));
+			result[lane_id].push(parseFloat(strikeAt.trim()));
 		}
 		return result;
 	}
 </script>
+
+<svelte:window on:keydown={onKeyDown} />
 
 <svelte:head>
 	<title>Home</title>
@@ -156,6 +195,21 @@
 				style="left: {lane.end_xy[0] - graphicSettings.hitpadOffset[0]}px; top:{lane.end_xy[1] -
 					graphicSettings.hitpadOffset[1]}px;"
 			/>
+		{/each}
+
+		{#each ballsInLane as laneBalls, laneId}
+			{#each laneBalls as balls}
+				<img
+					in:fly={{ y: -1400, duration: 1200 }}
+					out:fade
+					src={lanesGraphics[laneId].ball_graphic}
+					alt="hitpad"
+					class="hdpi block absolute"
+					style="left: {lanesGraphics[laneId].end_xy[0] -
+						graphicSettings.laneBallOffset}px; top:{lanesGraphics[laneId].end_xy[1] -
+						graphicSettings.laneBallOffset}px;"
+				/>
+			{/each}
 		{/each}
 	</div>
 
@@ -181,5 +235,3 @@
 		{/each}
 	</p>
 </section>
-
-<svelte:window on:keydown={onKeyDown} />
